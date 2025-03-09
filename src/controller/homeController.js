ae5f6108c5d56ae140f5e431a6c648cd2c40b819
Vũ Thế Vinh <147252615+trander-25@users.noreply.github.com>
2025-03-09 21:42:47 +0700

    Update homeController.js

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,15 +1,7 @@
 const handleHelloWord = (req, res) => {
   return res.render("home.ejs");
-<<<<<<< HEAD
-};
-
-module.exports = {
-  handleHelloWord,
-};
-=======
 }
 
 module.exports = {
   handleHelloWord,
 }
->>>>>>> f0b697e8591159634ab7cb2e31f8de6ec88bc359
