db95dbc36fb4b56f96bf859ff92118ace9b8aaad
trander <thevinh15925@gmail.com>
2025-04-22 23:30:22 +0700

    done 10.1.1

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,5 +1,7 @@
 import express from "express";
 import homeController from "../controller/homeController";
+import apiController from "../controller/apiController";
+
 const router = express.Router();
 
 const initWebRoutes = (app) => {
@@ -9,6 +11,8 @@ const initWebRoutes = (app) => {
   router.post("/delete-user/:id", homeController.handleDeleteUser);
   router.get("/update-user/:id", homeController.getUpdateUserPage);
   router.post("/user/update-user", homeController.handleUpdateUser);
+
+  router.get("/api/test-api", apiController.testApi);
 
   return app.use("/", router);
 };
